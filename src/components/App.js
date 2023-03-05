9ed969b53211c35127156488edf72824188161f2
Neha-Patel13 <122043318+Neha-Patel13@users.noreply.github.com>
2023-03-05 13:46:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,20 +12,38 @@ function App() {
  /**
   * code here
   */
-
-  return(
-    <div className="App">
-      <h1>How About Them Apples</h1>
-      <form>
-        <fieldset>
-          <label>
-            <p>First Name</p>
-            <input id='fname' name="name"  ref={fnameRef}/>
-            <br></br>
+ const fnameRef = useRef();
+  const emailRef = useRef();
+ const [error, setError] = useState(undefined);
+const[data, setData] = useState({fname: undefined, lname:undefined});
+ const change =()=>{
+  console.log(emailRef.current.value);
+  if(/^[a-zA-z0-9]+@[a-zA-z0-9]+\.[A-zA-z]+$/.test(emailRef.current.value)){
+    setError(undefined);
+   document.getElementById("submit").disable=false;
+  }else{
+   setError("Email is invalid");
+    document.getElementById("submit").disable=true;
+  }};
+ return(
+  <div className="App">
+  <h1>Hoe About Them Apples</h1>
+  <form onSubmit={(e)=>{e.preventDefault();
+                        setData({
+                         fname:fnameRef.current.value,
+                         lname:emailRef.current.value,
+                        });
+                       }}>
+                        <fieldset>
+                        <label>
+                        <p>First Name</p>
+                     <input Id='fname' name="name" ref={FnameRef}/>
+                      <br><br>
+  
             <p>Email</p>
-            <input id='lname' name="name"   ref={emailRef}/>
+            <input id='lname' name="name" onChange={change}  ref={emailRef}/>
             {error && <h2 style={{color: 'red'}}>{error}</h2>}
-          </label>
+          </label> 
         </fieldset>
 
         <button id='submit' type="submit">Submit</button>
